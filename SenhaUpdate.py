f9a32b75368c31726568ee69d24538efe99b9df6
Bruno Lima <bruno1638lima@hotmail.com>
2025-03-25 00:52:53 -0300

    Feat: Arrumando palavras!

--- SenhaUpdate.py
+++ SenhaUpdate.py
@@ -2,7 +2,7 @@ import secrets
 import string 
 
 letras = string.ascii_letters
-numeros = string.digits 
+numbers = string.digits 
 especial = string.punctuation
 
 while True:
@@ -11,33 +11,33 @@ while True:
     print("1 - Gerar senha")
     print("0 - Sair")
 
-    opcao_one = input("Escolha uma opção: ")
+    option_one = input("Escolha uma opção: ")
 
-    if opcao_one == "1":
+    if option_one == "1":
         size = int(input("Escolha o tamanho da senha: "))
 
-        opcao_two = input("Escolha caracteristicas para sua senha\n" 
+        option_two = input("Escolha caracteristicas para sua senha\n" 
                     "1-Digitos + Letras\n"
                     "2-Letras + Caractere Especial\n"
                     "3-Digitos + Letras + Caractere Especial\n"
                     "\nOpção: ")
-        if opcao_two == "1":
-            one_itens = letras + numeros
-            one_senha = [secrets.choice(one_itens)
+        if option_two == "1":
+            one_items = letras + numbers
+            one_senha = [secrets.choice(one_items)
                 for i in range (size)]
             one_senha = ''.join(one_senha)
             print(f"Sua senha gerada:\n{one_senha}")
         
-        elif opcao_two == "2":
-            two_itens = letras + especial
-            two_senha = [secrets.choice(two_itens)
+        elif option_two == "2":
+            two_items = letras + especial
+            two_senha = [secrets.choice(two_items)
                 for i in range (size)]
             two_senha = ''.join(two_senha)
             print(f"Sua senha gerada:\n{two_senha}")
         
-        elif opcao_two == "3":
-            three_itens = letras + numeros + especial
-            three_senha = [secrets.choice(three_itens)
+        elif option_two == "3":
+            three_items = letras + numbers + especial
+            three_senha = [secrets.choice(three_items)
                 for i in range (size)]
             three_senha = ''.join(three_senha)
             print(f"Sua senha gerada:\n{three_senha}")
@@ -45,7 +45,7 @@ while True:
             print("Opção inválida, tente novamente!")
         
 
-    elif opcao_one == "0":
+    elif option_one == "0":
         print("Encerrando o programa. Até logo!")
         break
     else:
