eeecb25a3af21e844eca8acdf48e187f65b11b36
Bruno Lima <bruno1638lima@hotmail.com>
2025-03-28 00:42:44 -0300

    Feat: tentanto com o tutorial ;)

--- password_gen.py
+++ password_gen.py
@@ -3,3 +3,24 @@
 # -Armazenar o usuário/e-mail
 # -Possibilitar a configuração do tamanho da senha 
 # - [BONUS]-Tocar música de fundo ao iniciar o programa
+
+import random
+import PySimpleGUI as sg
+
+class PassGen:
+    def __init__(self):
+        sg .theme('Black')
+        layout = [
+            [ sg.Text('Site/Software', size=(10,1)),
+             sg.Input(key='site', size=(20,1))],
+             [sg.Text('E-mail/Usuário', size=(10,1)),
+              sg.Input(key='usuário', size=(20,1))],
+              
+        ]
+    def Iniciar(self):
+        pass
+    def salvar_senha(self):
+        pass
+
+    gen = PassGen()
+    gen.Iniciar()
