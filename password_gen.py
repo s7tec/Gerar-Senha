07dcdeeb60d263f583873d78cb040551d0b9dab6
Bruno Lima <bruno1638lima@hotmail.com>
2025-03-28 00:14:01 -0300

    Feat: Novo gerador de senhas

---
+++ password_gen.py
@@ -0,0 +1,5 @@
+# Um programa de interface gráfica que deve ser capaz de:
+# -Armazenar o site/software p/ o qual a senha será gerado
+# -Armazenar o usuário/e-mail
+# -Possibilitar a configuração do tamanho da senha 
+# - [BONUS]-Tocar música de fundo ao iniciar o programa
